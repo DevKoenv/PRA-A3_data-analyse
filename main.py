7bd4d88e7457ddb9d18b59525c681c4e1e92996f
Gaming Mineblox <koenvanvlimmeren@gmail.com>
2022-12-12 11:12:50 +0000

    ♻️ Alle Choices Naar Funcites Verzet

--- main.py
+++ main.py
@@ -4,6 +4,73 @@ from datetime import datetime, timedelta
 
 data = pd.read_csv('VW.csv', sep=';')
 
+def choice1():
+    gemiddeldSalaris = data['Salaris_bruto'].mean()
+    print(f'Het gemiddelde salaris is: {gemiddeldSalaris}')
+
+    return(f'Het gemiddelde salaris is: {gemiddeldSalaris}')
+
+def choice2():
+    functie = input('Welke functie: ')
+    functie = functie[0].upper() + functie[1:]
+
+    functieFilter = (data['Functie'] == functie)
+
+    gemiddeldSalaris = data[functieFilter]["Salaris_bruto"].mean()
+    os.system('cls')
+    print(f'Het gemiddelde salaris voor functie {functie} is: {gemiddeldSalaris}')
+
+    return(f'Het gemiddelde salaris voor functie {functie} is: {gemiddeldSalaris}')
+
+def choice3():
+    willRetire = 0
+    for i in data['Geboortedatum']:
+        birthDate = datetime.strptime(i, '%d-%m-%Y')
+        today = datetime.today()
+        age = today.year - birthDate.year - ((today.month, today.day) < (birthDate.month, birthDate.day))
+        if age >= 65:
+            willRetire += 1
+
+    print(f'Aantal werknemers binnen 2 jaar met pensioen: {willRetire}')
+
+    return(f'Aantal werknemers binnen 2 jaar met pensioen: {willRetire}')
+
+def choice4():
+    chauffeurCount = 0
+    for i in data['Functie']:
+        if i == 'Chauffeur':
+            chauffeurCount += 1
+    print(f'Aantal chauffeurs: {chauffeurCount}')
+
+    return(f'Aantal chauffeurs: {chauffeurCount}')
+
+def choice5():
+    dateInServiceSorted = data.sort_values(by='Datum in dienst', ascending=False).head(10)
+    top10 = dateInServiceSorted[['Voornaam', 'Achternaam', 'Datum in dienst']].to_string(index=False)
+    print(f'Top 10 langst in dienst: \n{top10}')
+
+    return(f'Top 10 langst in dienst: \n{top10}')
+
+def choice6():
+    functie = input('Welke functie: ')
+    functie = functie[0].upper() + functie[1:]
+
+    afdeling = input('Welke afdeling: ')
+    afdeling = afdeling[0].upper() + afdeling[1:]
+
+    functieFilter = (data['Functie'] == functie)
+    afdelingFilter = (data['Afdeling'] == afdeling)
+
+    aantalMedewerkers = data[functieFilter & afdelingFilter].shape[0]
+
+    os.system('cls')
+    print(f'Aantal medewerkers met functie {functie} bij afdeling {afdeling}: {aantalMedewerkers}')
+
+    return(f'Aantal medewerkers met functie {functie} bij afdeling {afdeling}: {aantalMedewerkers}')
+
+
+
+
 isRunning = True
 while isRunning:
     os.system('cls')
@@ -27,65 +94,45 @@ while isRunning:
 
     elif choice == '1':
         os.system('cls')
-        gemiddeldSalaris = data['Salaris_bruto'].mean()
-        print(f'Het gemiddelde salaris is: {gemiddeldSalaris}')
+
+        choice1()
 
         input('\nDruk op enter om door te gaan...')
 
     elif choice == '2':
         os.system('cls')
-        functie = input('Welke functie: ')
-        functie = functie[0].upper() + functie[1:]
 
-        functieFilter = (data['Functie'] == functie)
-
-        gemiddeldSalaris = data[functieFilter]["Salaris_bruto"].mean()
-        os.system('cls')
-        print(f'Het gemiddelde salaris voor functie {functie} is: {gemiddeldSalaris}')
+        choice2()
 
         input('\nDruk op enter om door te gaan...')
 
     elif choice == '3':
         os.system('cls')
-        willRetire = 0
-        for i in data['Geboortedatum']:
-            birthDate = datetime.strptime(i, '%d-%m-%Y')
-            today = datetime.today()
-            age = today.year - birthDate.year - ((today.month, today.day) < (birthDate.month, birthDate.day))
-            if age >= 65:
-                willRetire += 1
 
-        print(f'Aantal werknemers binnen 2 jaar met pensioen: {willRetire}')
+        choice3()
+        
         input('\nDruk op enter om door te gaan...')
 
     elif choice == '4':
         os.system('cls')
-        chauffeurCount = 0
-        for i in data['Functie']:
-            if i == 'Chauffeur':
-                chauffeurCount += 1
-        print(f'Aantal chauffeurs: {chauffeurCount}')
+
+        choice4()
 
         input('\nDruk op enter om door te gaan...')
 
     elif choice == '5':
         os.system('cls')
-        dateInServiceSorted = data.sort_values(by='Datum in dienst', ascending=False).head(10)
-        top10 = dateInServiceSorted[['Voornaam', 'Achternaam', 'Datum in dienst']].to_string(index=False)
-        print(f'Top 10 langst in dienst: \n{top10}')
+
+        choice5()
 
         input('\nDruk op enter om door te gaan...')
 
     elif choice == '6':
         os.system('cls')
-        functie = input('Welke functie: ')
-        functie = functie[0].upper() + functie[1:]
+        
+        choice6()
 
-        afdeling = input('Welke afdeling: ')
-        afdeling = afdeling[0].upper() + afdeling[1:]
-
-        functieFilter = (data['Functie'] == functie)
-        afdelingFilter = (data['Afdeling'] == afdeling)
+        input('\nDruk op enter om door te gaan...')
 
         aantalMedewerkers = data[functieFilter & afdelingFilter].shape[0]
 
